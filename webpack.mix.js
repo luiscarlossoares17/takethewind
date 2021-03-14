3d0771c5610fe6daf54f6b6999381ef317ca33c0
Luis <luiscarlos7693@gmail.com>
2021-03-14 00:13:43 +0000

    Add new files with npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,32 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-merge-manifest');
+
+mix.options({
+    processCssUrls: false,
+    optimization: {
+        concatenateModules: false,
+        provideExports: false,
+        usedExports: false
+    }
+});
+
+mix.sourceMaps(true);
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
+
+
+    mix.extract();
+    mix.version();
